b051b83d1bce9f64e17a0a78902a11864e6a8938
Mehmed Kadric <mkadric3@etf.unsa.ba>
2018-01-26 01:00:46 +0100

    subtasks

---
+++ python/analiza.py
@@ -0,0 +1,51 @@
+from pandas import read_csv
+import pandas as pd
+import numpy as np
+
+
+dataset = read_csv('Titanic_dataset.csv', usecols=['Name', 'PClass', 'Age', 'Sex', 'Survived'])
+dataset.columns = ['Name', 'PClass', 'Age', 'Sex', 'Survived']
+dataset.head()
+print("Broj putnika: ", len(dataset))
+brojac = 0
+for line in dataset["Name"]:
+
+    if str(line)=="nan":
+       brojac += 1
+
+print("Broj putnika sa nepoznatim imenom: ", brojac)
+
+brojac = 0
+for line in dataset["PClass"]:
+
+    if str(line)=="nan":
+       brojac += 1
+
+print("Broj putnika sa nepoznatom putničkom klasom: ", brojac)
+
+brojac = 0
+for line in dataset["Age"]:
+
+    if str(line)=="nan":
+       brojac += 1
+
+print("Broj putnika sa nepoznatim godinama: ", brojac)
+
+
+
+brojac = 0
+for line in dataset["Sex"]:
+
+    if str(line)=="nan":
+       brojac += 1
+
+print("Broj putnika sa nepoznatim spolom: ", brojac)
+
+brojac = 0
+for line in dataset["Survived"]:
+
+    if str(line)=="nan":
+       brojac += 1
+
+print("Broj putnika sa nepoznatim ishodom (prezivjeli ili ne): ", brojac)
+
