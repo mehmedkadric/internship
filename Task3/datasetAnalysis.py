801fc0584c4b71a4701dc045989912cd1fc18a85
Mehmed Kadric <mkadric3@etf.unsa.ba>
2018-02-14 16:37:41 +0100

    code update

--- Task3/datasetAnalysis.py
+++ Task3/datasetAnalysis.py
@@ -2,17 +2,24 @@ import pandas as pd
 import numpy as np
 from pandas import read_csv
 from sklearn import preprocessing
-
+import io
+import request
 
 
 def loadDataset():
-    dataset = read_csv("secom.csv", header=None)
+    #url = "http://archive.ics.uci.edu/ml/machine-learning-databases/secom/secom.data"
+    #dataset = pd.read_csv(url, delimiter=" ", header=None)
+    #dataset.to_csv('secom.csv', header=None)
+    dataset = pd.read_csv('secom.csv', header=None)
     return dataset
 
 
 def main():
     ds = loadDataset()
     print(ds.describe())
+    print(ds.isnull().sum())
+    print(ds.size)
+
     return
 
 
