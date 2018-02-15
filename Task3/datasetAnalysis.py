406895d0cd64dbcc7aa0c4f71fa3738988ada9ce
Mehmed Kadric <mkadric3@etf.unsa.ba>
2018-02-15 23:34:43 +0100

    DBSCAN algorithm

--- Task3/datasetAnalysis.py
+++ Task3/datasetAnalysis.py
@@ -4,6 +4,8 @@ from pandas import read_csv
 from sklearn import preprocessing
 import io
 import request
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import MinMaxScaler
 
 
 def loadDataset():
@@ -14,19 +16,52 @@ def loadDataset():
     return dataset
 
 
-def preprocessing(ds):
+def loadLabels():
+    url = "http://archive.ics.uci.edu/ml/machine-learning-databases/secom/secom_labels.data"
+    labels = pd.read_csv(url, delimiter=" ", header=None)
+    labels.to_csv('secom_labels.csv', header=None)
+    #labels = pd.read_csv('secom_labels.csv', header=None)
+    return labels
+
+
+def preprocess(ds):
     for i in ds:
         ds[i].fillna((ds[i].mean()), inplace=True)
         if len(pd.unique(ds[i])) == 1:
             del ds[i]
+    x = ds.values
+    scaler = MinMaxScaler()
+    x_scaled = scaler.fit_transform(x)
+    ds = pd.DataFrame(x_scaled)
     return ds
+
+
+def DBscan(ds):
+    model = DBSCAN(eps=3, min_samples=20).fit(ds.values)
+    br = [0, 0]
+    for i in model.labels_:
+        if i == 0:
+            br[0] += 1
+        else:
+            br[1] +=1
+    print(br, pd.unique(model.labels_))
+    print(model.labels_[50:70])
+    return model
 
 
 def main():
     ds = loadDataset()
-    #print(ds.describe())
-    ds = preprocessing(ds)
+    ds = preprocess(ds)
+    model = DBscan(ds)
+    labels = loadLabels()
+    target = labels[:][0].values
+    for i, j in enumerate(target):
+        if j == -1:
+            target[i] = 0
+        else:
+            target[i] = -1
 
+    print(target[50:70])
     return
 
 
