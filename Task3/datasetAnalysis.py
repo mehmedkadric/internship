378016f320d9946ef4c2c9bd5241116b2ec09e54
Mehmed <Mehmed>
2018-02-19 04:34:57 +0100

    code updated

--- Task3/datasetAnalysis.py
+++ Task3/datasetAnalysis.py
@@ -6,6 +6,12 @@ import io
 import request
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from scipy import stats
+from scipy import spatial
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.feature_selection import VarianceThreshold
 
 
 def loadDataset():
@@ -17,10 +23,10 @@ def loadDataset():
 
 
 def loadLabels():
-    url = "http://archive.ics.uci.edu/ml/machine-learning-databases/secom/secom_labels.data"
+    """url = "http://archive.ics.uci.edu/ml/machine-learning-databases/secom/secom_labels.data"
     labels = pd.read_csv(url, delimiter=" ", header=None)
-    labels.to_csv('secom_labels.csv', header=None)
-    #labels = pd.read_csv('secom_labels.csv', header=None)
+    labels.to_csv('secom_labels.csv', header=None)"""
+    labels = pd.read_csv('secom_labels.csv', header=None)
     return labels
 
 
@@ -29,39 +35,75 @@ def preprocess(ds):
         ds[i].fillna((ds[i].mean()), inplace=True)
         if len(pd.unique(ds[i])) == 1:
             del ds[i]
+    del ds[0]
     x = ds.values
     scaler = MinMaxScaler()
     x_scaled = scaler.fit_transform(x)
     ds = pd.DataFrame(x_scaled)
-    return ds
+    return ds.loc[:, ds.var() > 0.03]
 
 
-def DBscan(ds):
-    model = DBSCAN(eps=3, min_samples=20).fit(ds.values)
-    br = [0, 0]
+def DBscan(ds, target):
+    mins = [4]
+    epses = [1.73]
+    for i in mins:
+        for j in epses:
+            model = DBSCAN(eps=j, min_samples=i)
+            model.fit_predict(ds.values)
+            print("trying...")
+            print(score(model, target)*100, pd.unique(model.labels_))
+            print(model.labels_[180:210])
+            print(target[180:210])
+            if score(model, target) >= 0.9337:
+                print(i, j)
+    ctr = [0, 0]
     for i in model.labels_:
-        if i == 0:
-            br[0] += 1
+        if i == -1:
+            ctr[0] += 1
         else:
-            br[1] +=1
-    print(br, pd.unique(model.labels_))
-    print(model.labels_[50:70])
+            ctr[1] += 1
+    print(ctr, pd.unique(model.labels_))
     return model
+
+
+def locModel(ds, target):
+    model = LocalOutlierFactor(n_neighbors=5)
+    y_pred = model.fit_predict(ds.values)
+    yModel = np.array(y_pred)
+    yTarget = np.array(target)
+    err = 0
+    for i, j in enumerate(yModel):
+        if j != yTarget[i]:
+            err += 1
+    print((len(yModel)-err)/len(yModel))
+
+
+def score(model, target):
+    yModel = np.array(model.labels_)
+    yTarget = np.array(target)
+    err = 0
+    for i, j in enumerate(yModel):
+        if j != yTarget[i]:
+            err += 1
+    return (len(yModel)-err)/len(yModel)
 
 
 def main():
     ds = loadDataset()
     ds = preprocess(ds)
-    model = DBscan(ds)
+    print(ds.shape)
     labels = loadLabels()
-    target = labels[:][0].values
+    target = labels[:][1].values
     for i, j in enumerate(target):
         if j == -1:
             target[i] = 0
         else:
             target[i] = -1
-
-    print(target[50:70])
+    #model = locModel(ds, target)
+    model = DBscan(ds, target)
+    #score(model, target)
+    #print(np.array(target))
+    #print(np.array(model.labels_))
     return
 
 
